0f222d6ab176e3654e8f09191a13d1b419516c03
Артур Протасов <arturprotasov@MacBook-Air-Artur.local>
2026-04-24 14:33:01 +0300

    Исправил цитаты

--- bot_init.py
+++ bot_init.py
@@ -1,8 +1,13 @@
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from bot_secrets import BOT_TOKEN
 from aiogram.fsm.storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN),
+)
 dp = Dispatcher(storage=storage)  # Создаем переменную диспетчера tg-бота
